4e5c86759ea010c4a4682e4494a4c3ac91299a5d
Gabriel <G.Bischof.inf22@stud.bbbaden.ch>
2024-08-23 13:52:42 +0200

    Navbar and Background have been added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,17 @@
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaUser } from "react-icons/fa";
+
 const Navbar = () => {
   return (
-    <nav className="bg-red-900 mb-20 flex items-center justify-between py-6">
+    <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
         <h1 className="text-white text-2xl font-bold">Gabriel Bischof</h1>
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <FaGithub />
+        <FaLinkedin />
+        <FaUser />
       </div>
     </nav>
   );
