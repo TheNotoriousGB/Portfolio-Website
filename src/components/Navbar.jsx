84ba04f1237767abc3a65548e63371c8f72882fa
Gabriel <G.Bischof.inf22@stud.bbbaden.ch>
2024-08-23 14:06:16 +0200

    Design Navbar finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,9 @@ const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <h1 className="text-white text-2xl font-bold">Gabriel Bischof</h1>
+        <h1 className="text-white text-xl font-medium">Gabriel Bischof</h1>
       </div>
-      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+      <div className="m-2 flex items-center justify-center gap-4 text-2xl">
         <FaGithub />
         <FaLinkedin />
         <FaUser />
