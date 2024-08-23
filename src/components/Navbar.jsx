c3458f7efbe80d632dfb7ae6c54aa7a87d888d4e
Gabriel <G.Bischof.inf22@stud.bbbaden.ch>
2024-08-23 10:13:31 +0200

    Navbar components added and formated with prettier

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,11 @@
-
-const Navbar = () => {  
-return (
-
-    <nav className = "bg-red-900 mb-20 flex items-center justify-between py-6">
-        <div className = "flex flex-shrink-0 items-center">
-            <h1 className="text-white text-2xl font-bold">Gabriel Bischof</h1>
-            </div>
-</nav>
-);
+const Navbar = () => {
+  return (
+    <nav className="bg-red-900 mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <h1 className="text-white text-2xl font-bold">Gabriel Bischof</h1>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
