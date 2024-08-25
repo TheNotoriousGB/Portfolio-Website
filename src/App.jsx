a6cd1f0d9bdeeec50e5eb2c636a0b7332dd4d7aa
Gabriel <G.Bischof.inf22@stud.bbbaden.ch>
2024-08-25 18:00:03 +0200

    Desing Änderungen

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import About from "./components/About";
 const App = () => {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
-      <div className="flixed top-0-z-10 h-full w-full"></div>
-      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      <div className="fixed top-0 z-10 h-full w-full bg-gradient-to-br from-black to-blue-600"></div>
+      <div className="absolute inset-0 z-0 h-full w-full bg-gradient-to-br from-black to-blue-600"></div>
 
-      <div className="container mx-auto px-8">
+      <div className="container mx-auto px-8 relative z-10">
         <Navbar />
         <Hero />  
         <About />
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,16 +1,63 @@
+import PROFILPIC from "../assets/20240823_135851000_iOS.jpg";
 
 const About = () => {
-  return (
-    <div className ="border-b border-neutral-900 pb-4">
-        <h2 className="my-20 tex-center text-4xl">Über <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">mich</span></h2>
-    </div>
-
-
-        
-
-        
-
-  )
+    return (
+        <div>
+            <div className="border-b border-neutral-900 pb-4 lg:mb-35">
+                <div className="flex flex-wrap">
+                    <div className="w-full lg:w-1/2">
+                        <div className="flex flex-col items-center lg:items-start">
+                            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
+                                Gabriel Bischof
+                            </h1>
+                            <span className="bg-gradient-to-r from-blue-900 via-black-500 to-blue-100 bg-clip-text text-4xl tracking-tight text-transparent">
+                                Informatikmittelschüler
+                            </span>
+                            <p>
+                                <span className="my-2 max-w-xl py-6 font-light tracking-tighter">
+                                    Ich bin ein Informatikmittelschüler aus der Schweiz und
+                                    begeistere mich für die Informatik und die Technologie. Ich bin
+                                    ein kreativer und leidenschaftlicher Entwickler, der sich
+                                    ständig weiterentwickelt und neue Technologien lernt.
+                                </span>
+                            </p>
+                        </div>
+                    </div>
+                    <div className="w-full lg:w-1/2">
+                        <div className="flex justify-center">
+                            <img
+                                src={PROFILPIC}
+                                alt="Gabriel Bischof"
+                                className="h-60 w-60 lg:h-80 lg:w-80"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="border-b border-neutral-900 pb-4">
+                <h2 className="my-20 tex-center text-4xl">
+                    Über{" "}
+                    <span className="bg-gradient-to-r from-blue-500 via-black-900 to-blue-100 bg-clip-text text-4xl tracking-tight text-transparent">
+                        mich
+                    </span>
+                </h2>
+                <div className="flex flex-wrap justify-center">
+                    <div className="flex-none w-64 rounded bg-slate-800 aspect-square rounded-lg flex items-center p-4 m-4 text-center">
+                        <h3 className="text-xl font-bold mb-2">Stärke 1</h3>
+                        <p className="text-gray-700">Beschreibung der Stärke 1</p>
+                    </div>
+                    <div className="flex-none w-64 rounded bg-slate-800 aspect-square rounded-lg flex items-center p-4 m-4 text-center">
+                        <h3 className="text-xl font-bold mb-2">Stärke 2</h3>
+                        <p className="text-gray-700">Beschreibung der Stärke 2</p>
+                    </div>
+                    <div className="flex-none w-64 rounded bg-slate-800 aspect-square rounded-lg flex items-center p-4 m-4 text-center">
+                        <h3 className="text-xl font-bold mb-2">Stärke 3</h3>
+                        <p className="text-gray-700">Beschreibung der Stärke 3</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default About
+export default About;
