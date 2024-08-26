55079029b17d22e4a8fcef5417ab9798b2d3d4e7
Gabriel <G.Bischof.inf22@stud.bbbaden.ch>
2024-08-26 08:40:06 +0200

    Created in about flexboxes added icons to the abou t section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,8 @@
 import PROFILPIC from "../assets/20240823_135851000_iOS.jpg";
+import { FaBoltLightning } from "react-icons/fa6";
+import { RiTeamLine } from "react-icons/ri";
+import { LuBrain } from "react-icons/lu";
+import { LuCalendarClock } from "react-icons/lu";
 
 const About = () => {
     return (
@@ -35,29 +39,53 @@ const About = () => {
                 </div>
             </div>
             <div className="border-b border-neutral-900 pb-4">
-                <h2 className="my-20 tex-center text-4xl">
+                <h2 className="my-20 text-center text-4xl">
                     Über{" "}
                     <span className="bg-gradient-to-r from-blue-500 via-black-900 to-blue-100 bg-clip-text text-4xl tracking-tight text-transparent">
                         mich
                     </span>
                 </h2>
                 <div className="flex flex-wrap justify-center">
-                    <div className="flex-none w-64 rounded bg-slate-800 aspect-square rounded-lg flex items-center p-4 m-4 text-center">
-                        <h3 className="text-xl font-bold mb-2">Stärke 1</h3>
-                        <p className="text-gray-700">Beschreibung der Stärke 1</p>
+                    <div className="flex-none w-64 bg-slate-800 aspect-square rounded-lg p-4 m-4 flex items-center justify-center text-center">
+                        <div className="flex flex-col items-center justify-center">
+                            <div className="m-2 text-6xl text-white">
+                                <FaBoltLightning />
+                            </div>
+                            <h3 className="text-xl font-bold mb-2 text-white">Stärke 1</h3>
+                            <p className="text-gray-400">Beschreibung der Stärke 1</p>
+                        </div>
                     </div>
-                    <div className="flex-none w-64 rounded bg-slate-800 aspect-square rounded-lg flex items-center p-4 m-4 text-center">
-                        <h3 className="text-xl font-bold mb-2">Stärke 2</h3>
-                        <p className="text-gray-700">Beschreibung der Stärke 2</p>
+                    <div className="flex-none w-64 bg-slate-800 aspect-square rounded-lg p-4 m-4 flex items-center justify-center text-center">
+                        <div className="flex flex-col items-center justify-center">
+                            <div className="m-2 text-6xl text-white">
+                                <RiTeamLine />
+                            </div>
+                            <h3 className="text-xl font-bold mb-2 text-white">Stärke 2</h3>
+                            <p className="text-gray-400">Beschreibung der Stärke 2</p>
+                        </div>
                     </div>
-                    <div className="flex-none w-64 rounded bg-slate-800 aspect-square rounded-lg flex items-center p-4 m-4 text-center">
-                        <h3 className="text-xl font-bold mb-2">Stärke 3</h3>
-                        <p className="text-gray-700">Beschreibung der Stärke 3</p>
+                    <div className="flex-none w-64 bg-slate-800 aspect-square rounded-lg p-4 m-4 flex items-center justify-center text-center">
+                        <div className="flex flex-col items-center justify-center">
+                            <div className="m-2 text-6xl text-white">
+                                <LuBrain />
+                            </div>
+                            <h3 className="text-xl font-bold mb-2 text-white">Stärke 3</h3>
+                            <p className="text-gray-400">Beschreibung der Stärke 3</p>
+                        </div>
+                    </div>
+                    <div className="flex-none w-64 bg-slate-800 aspect-square rounded-lg p-4 m-4 flex items-center justify-center text-center">
+                        <div className="flex flex-col items-center justify-center">
+                            <div className="m-2 text-6xl text-white">
+                                <LuCalendarClock />
+                            </div>
+                            <h3 className="text-xl font-bold mb-2 text-white">Stärke 4</h3>
+                            <p className="text-gray-400">Beschreibung der Stärke 4</p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     );
-}
+};
 
 export default About;
