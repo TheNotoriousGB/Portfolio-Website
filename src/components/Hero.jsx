e13fa00ca9a713d1534db72164b9e0995bb1c60e
Gabriel <G.Bischof.inf22@stud.bbbaden.ch>
2024-09-08 16:13:19 +0200

    Hero Section created.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,23 @@
 const Hero = () => {
 return (
-    <div>
-        
-    </div>
+ 
+    <section className="relative w-full h-auto bg-transparent overflow-hidden py-8">
+    <div className="relative w-full max-w-7xl mx-auto mb-12">
+      <div className="flex flex-col md:flex-row items-center justify-between px-4 md:px-8">
+        <div className="w-full md:w-1/2 h-full flex items-center justify-center p-4 md:p-8">
+          <div className="text-center md:text-left">
+            <h1 className="text-3xl md:text-4xl font-bold mb-4">
+            Willkommen auf meiner Portfolio Website
+            </h1>
+            <p className="text-base md:text-lg text-white-500">
+              Hier findest du wichtige Information und Projekte von mir.
+            </p>
+          </div>
+          </div>
+          </div>
+          </div>
+    </section>
+
 );
 };
 
