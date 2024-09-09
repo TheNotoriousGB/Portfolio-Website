c4ee822955e41ed770476e23c105e6e88769cca7
Gabriel <G.Bischof.inf22@stud.bbbaden.ch>
2024-09-09 10:33:24 +0200

    Änderungen

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,24 +1,22 @@
 const Hero = () => {
-return (
- 
-    <section className="relative w-full h-auto bg-transparent overflow-hidden py-8">
-    <div className="relative w-full max-w-7xl mx-auto mb-12">
-      <div className="flex flex-col md:flex-row items-center justify-between px-4 md:px-8">
-        <div className="w-full md:w-1/2 h-full flex items-center justify-center p-4 md:p-8">
-          <div className="text-center md:text-left">
-            <h1 className="text-3xl md:text-4xl font-bold mb-4">
-            Willkommen auf meiner Portfolio Website
-            </h1>
-            <p className="text-base md:text-lg text-white-500">
-              Hier findest du wichtige Information und Projekte von mir.
-            </p>
+  return (
+    <section className="relative w-full h-screen bg-transparent overflow-hidden py-8 flex items-center justify-center">
+      <div className="w-full max-w-7xl mx-auto">
+        <div className="flex flex-col md:flex-row items-center justify-between px-4 md:px-8">
+          <div className="w-full md:w-1/2 h-full flex items-center justify-center p-4 md:p-8">
+            <div className="text-center md:text-left">
+              <h1 className="pb-16 text-4xl tracking-tight lg:mt-10 lg:text-8xl">
+                Willkommen auf meiner Portfolio Website
+              </h1>
+              <p className="text-4xl tracking-tight">
+                Hier findest du wichtige Information und Projekte von mir.
+              </p>
+            </div>
           </div>
-          </div>
-          </div>
-          </div>
+        </div>
+      </div>
     </section>
-
-);
+  );
 };
 
 export default Hero;
